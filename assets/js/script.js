cc93dae696ac0b98b1abbe667a9ac4e70a9a0a44
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-04-01 06:06:09 -0400

    removed initial placeholders

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,8 @@
-$('.card-title').text('Project');
-$('p.card-text').text('This is a project');
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}

aa62600d1051a4b054e05a0749fc48f9e1d02658
Tyler-Phillips12 <Tyler-Phillips12@github.com>
2023-04-02 19:23:47 -0400

    removed slider function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,5 @@
-var slider = document.getElementById("myRadius");
-var output = document.getElementById("demo");
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
 
 $( function() {
   $( "#datepicker" ).datepicker();

6bacb121fab3f6598401092bb8f8d0de13a1f5f7
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-04-02 17:29:18 -0400

    centered elements, added datepicker and number radius select

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
-var slider = document.getElementById("myRange");
+var slider = document.getElementById("myRadius");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
+
+$( function() {
+  $( "#datepicker" ).datepicker();
+} );

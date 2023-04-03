b3230f0b753d7833eae37dd6bb9278590df5b1c0
Tyler-Phillips12 <Tyler-Phillips12@github.com>
2023-04-03 18:44:11 -0400

    Uploaded API key and API url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 // TODO: read user input
 // TODO: validate user input
+// TODO: Create variable for apikey and url
 // TODO: get data from API
 // TODO: create div for displaying data
 // TODO: display data
 // TODO: save data to local storage
 // TODO: create a "recent searches" list
+
+const apiKey = "4g4wrjxnxvv3mz2q3w7menj";
+const apiUrl = "http://api.amp.active.com/v2/search?facets=categoryName&per_page=0&api_key=4g4wrjxnxvv3mz2q3w7menj"
 
 $(function () {
   $("#datepicker").datepicker();

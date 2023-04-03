07a2895895acd579feb450ba6c3cca5d6e8b3113
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-04-03 19:37:46 -0400

    added from date and until date

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,40 @@
 // TODO: save data to local storage
 // TODO: create a "recent searches" list
 
-const apiKey = "GYyOSqBcm8hPEAfdpNrM7xPdTb9er8zT";
-const apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=GYyOSqBcm8hPEAfdpNrM7xPdTb9er8zT"
-
 fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
 
 $(function () {
-  $("#datepicker").datepicker();
+  $("#datepickerFrom").datepicker();
 });
+$(function () {
+  $("#datepickerUntil").datepicker();
+});
+
+// click event for search button
+$("#btnSearch").on("click", function (event) {
+  event.preventDefault();
+  var values = getValues();
+  // call api to get data using values
+  $.ajax({});
+});
+
+// get values from the form
+// return json with (element id, value) pairs
+function getValues() {
+  var searchTerm = $("#search").val();
+  var zipCode = $("#zipCode").val();
+  var radius = $("#myRadius").val();
+  var dateRange = {
+    from: $("#datepickerFrom").val(),
+    until: $("#datepickerUntil").val(),
+  };
+  return {
+    search: searchTerm,
+    zipCode: zipCode,
+    myRadius: radius,
+    datepicker: dateRange,
+  };
+}

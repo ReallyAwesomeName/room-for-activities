7f6183cd524ff8d07f602365b5c2388155da188f
Tyler-Phillips12 <Tyler-Phillips12@github.com>
2023-04-03 18:52:43 -0400

    fetch api

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,12 @@
 // TODO: create a "recent searches" list
 
 const apiKey = "4g4wrjxnxvv3mz2q3w7menj";
-const apiUrl = "http://api.amp.active.com/v2/search?facets=categoryName&per_page=0&api_key=4g4wrjxnxvv3mz2q3w7menj"
+const apiUrl = "http://api.amp.active.com/v2/search?query=running&category=event&start_date=2013-07-04..&near=San%20Diego,CA,US&radius=50&api_key=sqq35zvx6a8rgmxhy9csm8qj"
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
 
 $(function () {
   $("#datepicker").datepicker();
